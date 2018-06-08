544e4ad0eca6214aa0ea9e56f577ec8e59c33c35
booty <johnedmundrose@gmail.com>
2018-06-08 01:06:57 -0400

    Enable frozen string literals

--- lib/dotiw/version.rb
+++ lib/dotiw/version.rb
@@ -1,4 +1,5 @@
 # encoding: utf-8
+# frozen_string_literal: true
 
 module DOTIW
   VERSION = "4.0.0"
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,5 @@
 # encoding: utf-8
+# frozen_string_literal: true
 
 ROOT_PATH = File.join(File.dirname(__FILE__), '..')
 $:.unshift ROOT_PATH unless $:.include? ROOT_PATH
